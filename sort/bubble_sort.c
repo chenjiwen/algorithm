4fa8bfe6b03d1f27d47f1ccef10e01b388b29769
Chen Jiwen <cjiwen@qca.qualcomm.com>
2018-01-11 14:22:50 +0800

    implement bubble_sort

--- sort/bubble_sort.c
+++ sort/bubble_sort.c
@@ -1,3 +1,20 @@
 /*
  *bubble sort
+ *
  */
+
+void bubble_sort(int a[], int length)
+{
+    int i = 0, j = 0;
+    for(i = 0; i < length - 1; i++)
+        for(j = i; j < length - 1; j++)
+        {
+            int temp = 0;
+            if(a[j] > a[j + 1])
+            {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }  
+}
